377c56a122f73cc9d54bc062fcd916e6eaa56cdd
yanivt <34858407+yanivt@users.noreply.github.com>
2022-11-15 13:06:49 +0200

    Change notes

--- textAnalysis.py
+++ textAnalysis.py
@@ -11,7 +11,7 @@ from tkinter import font
 ## Written by Yaniv Tawily
 ## The Program:
 # My purpose in writing the program is to compare the similar parts of the Tanakh,
-# By that I mean that they are in two different versions for the same text.
+# By that I mean that they are the same text in two different versions in the Tanakh.
 # For example David's heroes who are shown in the II Samuel (ch 23) and in I Chronicles (ch 11).
 # For the demonstration I used both of them in the program, but it can be used on other texts as well.
 # The text I took from using the API library of Sefaria.
